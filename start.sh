ed5ba465e780383d4922cdab7dc02ddfa856fe04
huh-khamid <huh-khamid@users.noreply.github.com>
2026-05-25 01:09:32 +0500

    fix: clean start. sh

--- start.sh
+++ start.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 set -e
-alembbic upgrade head
-set -e
 alembic upgrade head
-set -e
-alembbic upgrade head
-set -e
-alembic upgrade head
-#20 writing cache image manifest sha256:8016651ddecab041302db363a6c0cd0be6737c0570d6d2d461156f303769a510 0.3s done
-
-
-
-
-
-==> Common ways to troubleshoot your deploy: http
-     0.0.0.0 --port ${PORT:-8000}\n
+exec uvicorn app.api.main:app --host 0.0.0.0 --port ${PORT:-8000}
