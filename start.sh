28397e1d9801dd14f3a7f865462e41ea6a62cfab
huh-khamid <huh-khamid@users.noreply.github.com>
2026-05-25 00:59:28 +0500

    fix: clean start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
-     #!/bin/bash                                                                                                                                                           
-     #!/bin/bash                                                                                                                                                                            91,768 tokens                           
-#!/bin/ba
-
-#!/bin/bas
+#!/bin/bash
 set -e
 alembic upgrade head
 exec uvicorn app.api.main:app --host 0.0.0.0 --port ${PORT:-8000}
